26c6e4905b719005072c493330f15ef27640fd23
fahd addi <fahd.addi@gmail.com>
2018-03-19 11:55:19 +0000

    modification du code pour n'uploader que les fichiers du format XLS et XLSX

--- routes/siteshs.js
+++ routes/siteshs.js
@@ -11,7 +11,15 @@ var storage = multer.diskStorage({
   }
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({
+  storage: storage,
+  fileFilter : function(req, file, callback) { //file filter
+    if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+      return callback(new Error('Wrong extension type'));
+    }
+    callback(null, true);
+  }
+}).single("file");
 
 
 
@@ -21,9 +29,16 @@ route.get("/upload",(req,res,next)=>{
   res.render("siteshs/upload");
 });
 
-route.post('/upload',upload.single("file"),function(req,res,next){
-    console.log(req.file);
-    res.render("siteshs/type-du-rapport");
+route.post('/upload',function(req,res,next){
+  upload(req,res,function(err){
+    if(err){
+      res.json({error_code:1,err_desc:err});
+      return;
+    }
+    res.send("siteshs/type-du-rapport");
+  });
+  //  console.log(req.file);
+  //  res.render("siteshs/type-du-rapport");
 })
 
 
